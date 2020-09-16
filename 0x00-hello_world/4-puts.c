3bc57497adc3c647973b71e513b854a34553ed1d
CarolinaHV <carolina.hv02@gmail.com>
2020-09-16 22:11:42 +0000

    Using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - puts message
+ * Use function puts, dont use printf
+ * Return: Value 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
