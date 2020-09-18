32ecf6af7a30c8c25c263d42d8477bd5ed2ebcf3
CarolinaHV <carolina.hv02@gmail.com>
2020-09-18 02:49:47 +0000

    prints numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - using putchar
+ *
+ * This program prints the numbers from 00 to 99,
+ * the numbers must be separated by ',' followed by a space.
+ *
+ * Return: Value 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 48; m < 58; m++)
+		{
+			putchar(n);
+			putchar(m);
+
+			if (m < 58)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
