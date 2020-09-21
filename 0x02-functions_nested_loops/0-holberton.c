a66cd5c3b42215cc3a6a1cd3db49f9ffc8606f00
CarolinaHV <carolina.hv02@gmail.com>
2020-09-21 20:36:23 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - _putchar
+ *
+ * This program prints Holberton, followed by a new line.
+ *
+ * Return: Value 0
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+
+	return (0);
+}
