938daa97684e2af6f0dcf04512bd359045fcc117
CarolinaHV <carolina.hv02@gmail.com>
2020-09-23 03:04:07 +0000

    24 Jack Bauer timer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,34 @@
 #include "holberton.h"
 /**
- * main - 
+ * jack_bauer - This function that prints every minute of the day,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: Value 0
+ */
+void jack_bauer(void)
+{
+	int hora;
+	int min;
+	int day;
+
+	hora = 0;
+	while (hora <= 23)
+	{
+		min = 0;
+		while (min <= 59)
+		{
+			day = hora / 10;
+			_putchar('0' + day);
+			day = hora % 10;
+			_putchar('0' + day);
+			_putchar(':');
+			day = min / 10;
+			_putchar('0' + day);
+			day = min % 10;
+			_putchar('0' + day);
+			_putchar('\n');
+			min++;
+		}
+		hora++;
+	}
+}
