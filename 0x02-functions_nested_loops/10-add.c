494b470c03a78b043b26e75dc62c24191ac7f85e
CarolinaHV <carolina.hv02@gmail.com>
2020-09-23 04:50:45 +0000

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - This function add two ontegers and return the result.
+ * @numA: First number
+ * @numB: Second number
+ *
+ * Return: Value 0
+ */
+int add(int numA, int numB)
+{
+	int resul;
+
+	resul = numA + numB;
+
+	return (resul);
+}
