25adb464c3933613d47b63adfd6e99b81a3439bb
CarolinaHV <carolina.hv02@gmail.com>
2020-09-29 00:04:07 +0000

    Swap two integer numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - This function  swaps the values of two integers
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: Value 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *b - *a;
+}
