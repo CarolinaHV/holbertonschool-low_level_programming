65cb96bdd81b3ffbc4f5e50c60e47b4e6716d9e3
CarolinaHV <carolina.hv02@gmail.com>
2020-10-02 01:08:52 +0000

    Reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * reverse_array - This function reverses the content
+ * of an array of integers.
+ * @a: Array of integers
+ * @n: Number of elements of the array
+ * Return: Value 0
+ */
+void reverse_array(int *a, int n)
+{
+	int rev;
+	int i = 0;
+
+	while (a[n] != '\0')
+	{
+		n++;
+	}
+
+	for (rev = 0; rev < n / 2; rev++)
+	{
+		i = a[rev];
+		a[rev] = a[n - 1 - rev];
+		a[n - 1 - rev] = i;
+	}
+}
