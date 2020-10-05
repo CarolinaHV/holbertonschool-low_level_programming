64c4f840584cb2f1c6c6a97af8bd283cc0f365c3
CarolinaHV <carolina.hv02@gmail.com>
2020-10-05 21:16:09 +0000

    Function memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memcpy - This function copies memory area.
+ * @dest: char destiny
+ * @src: char source
+ * @n: number bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
