e2e4b32c7323e64cfba32671e20e472e0aaeb6f8
CarolinaHV <carolina.hv02@gmail.com>
2020-10-07 03:00:51 +0000

    Function strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strstr - This function locates a substring
+ * @haystack: String
+ * @needle: Substring
+ * Return: Value 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h = haystack;
+	char *n = needle;
+
+	while (h != '\0')
+	{
+		if (h == n)
+		{
+			return (h);
+		}
+		h++;
+	}
+	return (NULL);
+}
