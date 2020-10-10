26a2f4b2a1d88f91232ec02a3461e65005342b9c
CarolinaHV <carolina.hv02@gmail.com>
2020-10-10 02:15:17 +0000

    argc-argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This program prints all arguments it receives.
+ * print one argument per line, ending with a new line
+ * @argc: int
+ * @argv: array
+ * Return: Value 0
+ */
+int main (int argc, char *argv[])
+{
+	int i = 0;
+	while(i < argc)
+	{
+		printf("%s", argv[i]);
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
