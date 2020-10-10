cc2ba47ecc735bc26c949ea161684a975500defb
CarolinaHV <carolina.hv02@gmail.com>
2020-10-10 00:12:42 +0000

    argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program prints its name
+ * @argc: int
+ * @argv: array
+ * Return: value 0
+ */
+int main (int argc, char *argv[])
+{
+	int i = 0;
+	while(i < argc)
+	{
+		printf("%s \n", argv[i]);
+		i++;
+	}
+	return (0);
+}
