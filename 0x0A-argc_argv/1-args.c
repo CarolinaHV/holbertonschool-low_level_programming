b4a7400f9d39735d264ded7545739ed08c289429
CarolinaHV <carolina.hv02@gmail.com>
2020-10-11 03:38:31 +0000

    argc-argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,11 @@
  * @argv: string
  * Return: value 0
  */
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 	{
-		printf("%d \n", argc -1);
+		printf("%d \n", argc - 1);
 	}
 	return (0);
 }
