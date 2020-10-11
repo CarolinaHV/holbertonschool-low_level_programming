1b7befe2e5a0d837536002897dee9ec355384c21
CarolinaHV <carolina.hv02@gmail.com>
2020-10-11 06:53:18 +0000

    Modification

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array
  * Return: value 0
  */
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s \n", argv[0]);
 	return (0);
