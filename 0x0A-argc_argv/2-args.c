1539f23864fd69a1f6de0a30d1e01bd522278d92
CarolinaHV <carolina.hv02@gmail.com>
2020-10-11 03:44:07 +0000

    argc-argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - This program prints all arguments it receives.
  * print one argument per line, ending with a new line
@@ -6,13 +6,14 @@
  * @argv: array
  * Return: Value 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
-	while(i < argc)
+
+	while (i < argc)
 	{
 		printf("%s", argv[i]);
-		putchar('\n');
+		_putchar('\n');
 		i++;
 	}
 	return (0);
