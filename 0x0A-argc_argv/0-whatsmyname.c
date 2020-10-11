9d28b852ecd4a00b95b3f9427938c99a66f96238
CarolinaHV <carolina.hv02@gmail.com>
2020-10-11 03:25:36 +0000

    argc-argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
  * @argv: array
  * Return: value 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
-	while(i < argc)
+
+	while (i < argc)
 	{
 		printf("%s \n", argv[i]);
 		i++;
