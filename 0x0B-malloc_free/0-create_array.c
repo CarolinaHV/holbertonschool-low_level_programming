489b293db839604c322527ce426858b4e4893ff3
CarolinaHV <carolina.hv02@gmail.com>
2020-10-13 21:31:24 +0000

    Array of chars - Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * create_array - This function creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: unsigned int
+ * @c: char
+ * Return: 
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		str = malloc(size * sizeof(char));
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
