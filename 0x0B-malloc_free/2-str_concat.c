5778fce98ea7f6fc4ce682e3bba3fbc7e4fb2d82
CarolinaHV <carolina.hv02@gmail.com>
2020-10-14 04:22:51 +0000

    Function concat - malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * str_concat - This function concatenates two strings.
+ * @s1: String one
+ * @s2: String two
+ * Return: conc
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	char *conc;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	conc = malloc(((i + j) + 1) * sizeof(char));
+	if (conc == NULL)
+	{
+		return (NULL);
+	}
+	for (; k < i ; k++)
+	{
+		conc[k] = *s1++;
+	}
+	for (k = i; k <= (i + j) + 2; k++)
+	{
+		conc[k] = *s2++;
+	}
+	return (conc);
+}
