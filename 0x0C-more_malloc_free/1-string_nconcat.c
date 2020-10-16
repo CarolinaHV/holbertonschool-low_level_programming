9ed18b043cae6b946c2e7cdd26fceb95ac9266aa
CarolinaHV <carolina.hv02@gmail.com>
2020-10-16 05:35:19 +0000

    concatenation malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+/**
+ * string_nconcat - This function concatenates two strings.
+ * @s1: First string
+ * @s2: Second string
+ * @n: number of bytes
+ * Return: arr
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	char *conc;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (n >= j)
+	{
+		n = j;
+	}
+	else
+	{
+		j = n;
+	}
+
+	conc = malloc(sizeof(unsigned int) * ((i + j) + 1));
+
+	if (conc == NULL)
+	{
+		return (NULL);
+	}
+	for (; s1[k] != '\0' ; k++)
+	{
+		conc[k] = s1[k];
+	}
+	for (n = 0; n < j; n++, k++)
+	{
+		conc[k] = s2[n];
+	}
+	s2[n] = '\0';
+	return (conc);
+}
