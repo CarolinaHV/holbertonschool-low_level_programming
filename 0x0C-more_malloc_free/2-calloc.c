d7e797689589c7102bab5aa2cbb3fce2c0cd8240
CarolinaHV <carolina.hv02@gmail.com>
2020-10-16 03:09:45 +0000

    Calloc - malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _calloc - This function allocates memory for an array, using malloc.
+ * @nmemb: Number of elements
+ * @size: Number of bytes
+ * Return: arr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int cal;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	cal = nmemb * size;
+
+	arr = malloc(cal);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < cal; i++)
+	{
+		arr[1] = 0;
+	}
+	return (arr);
+}
