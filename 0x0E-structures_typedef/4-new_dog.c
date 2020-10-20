76b37b82c8e1534835c9b5d9bb79cb9fd51be913
CarolinaHV <carolina.hv02@gmail.com>
2020-10-20 03:48:53 +0000

    Copy function struct dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - This function creates a new dog.
+ * @name: type char
+ * @age: type float
+ * @owner: type char
+ * Return: newInf
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int newN = 0;
+	int newO = 0;
+	int k;
+	dog_t *newInf;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	newInf = malloc(sizeof(dog_t));
+	if (newInf == NULL)
+		return (NULL);
+
+	while (name[newN] != '\0')
+		newN++
+	while (owner[newO] != '\0')
+		newO++;
+
+	newInf->name = malloc(sizeof(char) * newN + 1);
+	newInf->owner = malloc(sizeof(char) * newO + 1);
+	if (newInf->name == NULL || newInf->owner == NULL)
+	{
+		free(newInf->name);
+		free(newInf->owner);
+		free(newInf);
+		return (NULL);
+	}
+	for (k = 0; k < newN; k++)
+	{
+		newInf->name[k] = name[k];
+	}
+	for (k = 0; k < newO; k++)
+	{
+		newInf->owner[k] = owner[k];
+	}
+	newInf->age = age;
+
+	return (newInf);
+}
