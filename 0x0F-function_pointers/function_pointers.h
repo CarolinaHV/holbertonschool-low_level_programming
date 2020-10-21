d870cfbbe037b502ce3d1f667c6f7a9826a43182
CarolinaHV <carolina.hv02@gmail.com>
2020-10-21 22:45:49 +0000

    Prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,3 +14,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
