2d7e770003e449769bc6135e70ac5a07cf3a6bdb
CarolinaHV <carolina.hv02@gmail.com>
2020-10-22 05:01:16 +0000

    Calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,8 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
+	int a;
+	int b;
 
 	if (argc != 4)
 	{
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	num1 = atoi(argv[2]);
-	num2 = atoi(argv[2]);
+	a = atoi(argv[2]);
+	b = atoi(argv[2]);
 
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
 	{
