6c7a110ac682eeb2018d66ad79782dca56198148
CarolinaHV <carolina.hv02@gmail.com>
2020-10-22 05:07:20 +0000

    Calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,14 +22,15 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
 	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }
