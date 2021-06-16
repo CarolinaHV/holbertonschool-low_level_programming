4b44b8777bf1ce5f17c3034ef929bd1a836aa66e
CarolinaHV <carolina.hv02@gmail.com>
2021-06-16 06:12:06 +0000

    Here we go again.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,4 +12,9 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int finder(int *array, int init, int finish, int value);
 
+int finder(int *array, size_t size, size_t low, size_t high, int value);
+void print_array(int *array, size_t low, size_t high);
+int find_lowest(int *array, int mid, int value, int low);
+
+
 #endif
